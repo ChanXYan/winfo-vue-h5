23a72ba9d63d1e74c20267e671b823bcc5831eed
王艺泳 <270357051@qq.com>
2020-08-24 17:03:02 +0800

    change: 全局引用 &  样式引用

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,8 @@
 </template>
 
 <style lang="scss">
+@import url('./assets/style/reset.less');
+
 #app {
   font-family: PingFang SC, Avenir, Helvetica, Arial, sans-serif;
   font-size: 16px;
--- src/main.js
+++ src/main.js
@@ -2,13 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import storage from './storage/storage'
 
 // vant 按需加载
 import './utils/vantImport'
 import 'lib-flexible/flexible' // px2rem适配
 import './rem'
 
+//引入iconfont
+import './assets/fonts/iconfont.css'
 Vue.config.productionTip = false
+
+// 已经封装过的localStorage
+window.ls = new storage()
 
 new Vue({
   router,
