28247b24b69de1da7c18c8dda6c2cccaccdb76c6
王艺泳 <270357051@qq.com>
2020-08-26 15:44:25 +0800

    change:  2个接口请求较慢 因此单独配置timeout为60s

--- src/api/modules/checkReport.js
+++ src/api/modules/checkReport.js
@@ -6,12 +6,15 @@ export default {
   // fsc
   flagStateControlQuery (data) {
     return service.get(
-      `${preUrl}sz-mini-program/nationReport/flagStateControlQuery`, data
-    )
+      `${preUrl}sz-mini-program/nationReport/flagStateControlQuery`, data, {}, true, {
+      timeout: 60000
+    })
   },
   // 现场监督
   siteSupervisionQuery (data) {
-    return service.get(`${preUrl}sz-mini-program/nationReport/siteSupervisionQuery`, data)
+    return service.get(`${preUrl}sz-mini-program/nationReport/siteSupervisionQuery`, data, {}, true, {
+      timeout: 60000
+    })
   },
   // 历史记录
   searchHistory () {
