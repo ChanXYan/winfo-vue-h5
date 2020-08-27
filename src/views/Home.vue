ea4aaca748fb6c501806b58a536b8cb555add5d2
王艺泳 <270357051@qq.com>
2020-08-27 17:32:31 +0800

    feat： 首页展示已实现的页面入口

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="home">home</div>
+  <div class="home">
+    <div class="list">
+      <div
+        v-for="(item,index) in list"
+        :key="'item'+index"
+        class="item"
+        @click="clickItem(item)"
+      >{{item.name}}</div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,7 +17,30 @@ export default {
   name: 'Home',
   data () {
     return {
-
+      list: [{
+        url: '/checkReportSearch',
+        name: '报告查询',
+        params: ''
+      }, {
+        url: '/navWarning/1',
+        name: '航行警告',
+        params: ''
+      }, {
+        url: '/navWarning/2',
+        name: '航行通告',
+        params: ''
+      }, {
+        url: '/airInfo',
+        name: '气象信息',
+        params: ''
+      }]
+    }
+  },
+  methods: {
+    clickItem (item) {
+      this.$router.push({
+        path: item.url,
+      })
     }
   },
   components: {},
@@ -21,6 +53,32 @@ export default {
 <style scope lang="less">
 .home {
   width: 750px;
-  height: 100px;
+  min-height: 100vh;
+  background: #eee;
+  overflow: hidden;
+  .list {
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    margin: 30px;
+    .item {
+      width: 210px;
+      height: 150px;
+      line-height: 150px;
+      border-radius: 25px;
+      margin-bottom: 20px;
+      text-align: center;
+      font-size: 28px;
+      background: #fff;
+      margin-right: 30px;
+    }
+    .item:nth-child(3n + 3) {
+      margin-right: 0;
+    }
+    .item:active {
+      background: cornflowerblue;
+      color: #fff;
+    }
+  }
 }
 </style>
