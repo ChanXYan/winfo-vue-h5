17ba9b7fba51779d73a057bfe1a633f1c55edffe
王艺泳 <270357051@qq.com>
2020-09-02 13:17:44 +0800

    change： 暂存 （选择器组件封装未完成）

--- src/views/orgQuery/test.vue
+++ src/views/orgQuery/test.vue
@@ -1,44 +1,186 @@
 
 <template>
   <div class="container">
-    <div class="picker" @click="show= true">
-      <span class="iconfont icondidianshaixuan"></span>
-      {{keyword ||'请选择'}}
-      <span class="iconfont icondown ml10"></span>
+    <div>
+      <button @click="show1 =!show1">显示多选</button>
+      {{values}}
     </div>
-    <van-popup v-model="show" position="bottom">
-      <van-picker title show-toolbar :columns="columns" @confirm="onConfirm" @cancel="show=false" />
-    </van-popup>
 
-    <van-divider :style="{ color: '#1989fa', borderColor: '#1989fa', padding: '0 16px' }">文字</van-divider>
+    <div class="mt30">
+      <button @click="show2 =!show2">显示二维单选选</button>
+      {{values1}}
+    </div>
+
+    <!-- <div class="mt30">
+      <button @click="show3 =!show3">显示一维单选</button>
+      {{values2}}
+    </div>-->
+    <queryPicker
+      :show="show1"
+      :type="3"
+      :list="list1"
+      :values="values"
+      @close="show1=false"
+      @onComfirm="onComfirm1"
+    ></queryPicker>
+    <queryPicker
+      :show="show2"
+      :type="2"
+      :list="list2"
+      :values="values1"
+      @close="show2=false"
+      @onComfirm="onComfirm2"
+    ></queryPicker>
+    <!-- <queryPicker :show="show3" :type="1" :list="list3" :values="values2" @close="show3=false"></queryPicker> -->
   </div>
 </template>
 
 <script>
-
+import queryPicker from '../../components/queryPicker'
 export default {
   components: {
+    queryPicker
   },
   data () {
     //这里存放数据
     return {
-
-      show: false,
-
-      loading: false,
-      finished: false,
-      keyword: '',
-      orgNameObj: {},
-      columns: ['杭州', '宁波', '温州', '绍兴', '湖州', '嘉兴', '金华', '衢州'], //picker数组
-
+      show1: false,
+      show2: false,
+      show3: false,
+      values: [1, 2],
+      values1: ['2'],
+      values2: ['1'],
+      list1: [
+        {
+          label: '合格证',
+          value: 1
+        }, {
+          label: '适任证',
+          value: 2
+        }, {
+          label: '健康证明',
+          value: 3
+        }, {
+          label: '海员证',
+          value: 4
+        }, {
+          label: '技术资格证',
+          value: 5
+        }, {
+          label: '海船船员内河航线行驶资格证明',
+          value: 6
+        }, {
+          label: '特定航线江海直达船舶船员行驶资格证明',
+          value: 7
+        }
+      ],
+      list2: [
+        {
+          title: '海船考试类型',
+          list: [{
+            label: '船长和高级船员适任考试',
+            value: '1'
+          }, {
+            label: '普通和高级船员适任考试',
+            value: '2'
+          }, {
+            label: 'GMDSS适任考试',
+            value: '3'
+          }, {
+            label: '非自航船适任考试',
+            value: '4'
+          }, {
+            label: '海港引航员适任考试',
+            value: '5'
+          }]
+        },
+        {
+          title: '内河考试类型',
+          list: [{
+            label: '内河全国统考（长江）',
+            value: '6'
+          }, {
+            label: '内河全国统考（珠江）',
+            value: '7'
+          }, {
+            label: '内河全国统考（黑龙江）',
+            value: '8'
+          }, {
+            label: '内河非全国统考',
+            value: '9'
+          }, {
+            label: '内河引航员考试',
+            value: '10'
+          }]
+        }
+      ],
+      list3: [
+        {
+          label: '航海英语',
+          value: 1
+        }, {
+          label: '船舶操纵与避碰',
+          value: 2
+        }, {
+          label: '航海学',
+          value: 3
+        }, {
+          label: '船舶管理（驾驶）',
+          value: 4
+        }, {
+          label: '船舶结构与货运',
+          value: 5
+        }, {
+          label: '船舶辅机',
+          value: 6
+        }, {
+          label: '航海英语',
+          value: 1
+        }, {
+          label: '船舶操纵与避碰',
+          value: 2
+        }, {
+          label: '航海学',
+          value: 3
+        }, {
+          label: '船舶管理（驾驶）',
+          value: 4
+        }, {
+          label: '船舶结构与货运',
+          value: 5
+        }, {
+          label: '船舶辅机',
+          value: 6
+        }, {
+          label: '航海英语',
+          value: 1
+        }, {
+          label: '船舶操纵与避碰',
+          value: 2
+        }, {
+          label: '航海学',
+          value: 3
+        }, {
+          label: '船舶管理（驾驶）',
+          value: 4
+        }, {
+          label: '船舶结构与货运',
+          value: 5
+        }, {
+          label: '船舶辅机',
+          value: 6
+        }
+      ]
     };
   },
   computed: {},
   methods: {
-
-
-
-
+    onComfirm1 (o) {
+      this.values = [...o]
+    },
+    onComfirm2 (o) {
+      this.values1 = [...o]
+    }
   },
   created () {
 
@@ -65,18 +207,15 @@ export default {
   height: 100vh;
   background: #f5f8fa;
   color: #333;
-  // .picker {
-  //   display: flex;
-  //   align-items: center;
-  //   font-size: 30px;
-  //   line-height: 90px;
-  //   background: #fff;
-  //   padding: 0 30px;
-  //   .icondidianshaixuan {
-  //     font-size: 40px;
-  //     color: #cfdae6;
-  //     margin-right: 10px;
-  //   }
-  // }
+  .listWrap {
+    width: 100%;
+    height: 50px;
+    overflow: hidden;
+    .list {
+      overflow: auto;
+      width: 100%;
+      height: 100%;
+    }
+  }
 }
 </style>
